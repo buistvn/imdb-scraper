cd18c1d5219dab2360a628e16b51f52481dce2fe
buistvn <buist@oregonstate.edu>
2020-10-04 18:01:49 -0700

    Added sorting and writing to an output file

--- film_data.py
+++ film_data.py
@@ -1,20 +1,14 @@
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-
-def get_film_data(link: str) -> None:
-    page_url = link
-    
-    uClient = urlopen(page_url)
-    page_html = uClient.read()
-    uClient.close()
-
-    page_soup = BeautifulSoup(page_html, "html.parser")
-
-    film_title = page_soup.find("h1", {"class":"headline-1"}).text
-    film_year = page_soup.find("small", {"class":"number"}).a.text
-    film_director = page_soup.find("span", {"class":"prettify"}).text
-    film_overview = page_soup.find("div", {"class":"truncate"}).p.text
-
-    print(film_title + " (" + film_year + ") directed by " + film_director)
-    print(film_overview)
-    print("")
+class FilmData:
+    def __init__(
+        self,
+        title: str = " ",
+        director: str = " ",
+        overview: str = " ",
+        rating: float = 0,
+        popularity: int = 0,
+    ):
+        self.title = title
+        self.director = director
+        self.overview = overview
+        self.rating = rating
+        self.popularity = popularity
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-from film_data import get_film_data
+from scrape import scrape
 
 
 def main() -> None:
-    page_url = "https://letterboxd.com/films/"
+    page_url = "https://www.imdb.com/chart/moviemeter/?ref_=nv_mv_mpm"
 
     uClient = urlopen(page_url)
     page_html = uClient.read()
@@ -13,13 +13,37 @@ def main() -> None:
 
     page_soup = BeautifulSoup(page_html, "html.parser")
 
-    list_items = page_soup.find_all("li", {"class":"listitem"})
+    list_container = page_soup.find("tbody", {"class":"lister-list"})
+    list_items = list_container.find_all("tr", limit=10)
     
-    for list_item in list_items:
-        suffix = list_item.div["data-target-link"]
-        link = "https://letterboxd.com" + suffix
-        get_film_data(link)
+    film_list = []
+
+    for index, list_item in enumerate(list_items, start=1):
+        suffix = list_item.td.a["href"]
+        link = "https://www.imdb.com" + suffix
+
+        film_list.append(scrape(link, index))
+
+    f = open("film_data.txt", "w")
     
+    f.write("---------- SORTED BY POPULARITY ---------- \n \n")
+    for film in film_list:
+        f.write(film.title + "directed by " + film.director + "\n")
+        f.write(film.overview + "\n")
+        f.write("Average Rating: " + str(film.rating) + "\n")
+        f.write("Popularity: " + str(film.popularity) + "\n")
+        f.write("\n")
+
+    film_list.sort(key=lambda x: x.rating, reverse=True)
+    f.write("---------- SORTED BY AVERAGE RATING ---------- \n \n")
+    for film in film_list:
+        f.write(film.title + "directed by " + film.director + "\n")
+        f.write(film.overview + "\n")
+        f.write("Average Rating: " + str(film.rating) + "\n")
+        f.write("Popularity: " + str(film.popularity) + "\n")
+        f.write("\n")
+
+    f.close()
 
 if __name__ == "__main__":
     main()
